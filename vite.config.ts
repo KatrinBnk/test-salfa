545fafbbc5c0284b9a5e29e0dd097879f31a8368
KatrinBnk <yekaterinabanko@gmail.com>
2025-01-04 22:23:29 +0300

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: 'test-salfa', //NOTE: для публикации на gh pages
-  plugins: [react()],
-})
